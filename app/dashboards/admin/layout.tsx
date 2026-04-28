e7e6d782f3f7bc176df90f7c2287bd8ba859b204
Shek Rasel <swe.rasel@gmail.com>
2026-04-28 17:37:15 +0600

    admin layout done

--- app/dashboards/admin/layout.tsx
+++ app/dashboards/admin/layout.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import AdminNavbar from "@/app/dashboards/admin/_components/admin-navbar";
+import AdminSidebar from "@/app/dashboards/admin/_components/admin-sidebar";
 
-const AdminLayout = () => {
-  return <div>AdminLayout</div>;
+const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <aside className="h-screen w-14 shrink-0 bg-primary md:w-44 lg:w-52 xl:w-64">
+        <AdminSidebar />
+      </aside>
+
+      <div className="flex h-screen min-w-0 flex-1 flex-col">
+        <nav className="h-12 shrink-0 bg-primary md:h-18">
+          <AdminNavbar />
+        </nav>
+
+        <main className="flex-1 overflow-y-auto p-4 md:p-6 w-full xl:w-5xl 2xl:w-7xl mx-auto">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default AdminLayout;
