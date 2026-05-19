d9bb9895d9100c5765efc02a7e2a9572b6cefd95
Shek Rasel <swe.rasel@gmail.com>
2026-05-19 12:49:07 +0600

    get valid image utils

---
+++ utils/image.utils.ts
@@ -0,0 +1,15 @@
+import { IMAGE } from "@/constants/image.path";
+
+export const getValidImage = (imageUrl?: string | null) => {
+  const fallbackImage = IMAGE.fallback_image;
+
+  if (!imageUrl) return fallbackImage;
+
+  const invalidImagePatterns = ["favicon", ".ico", "sn_favicon"]; //you can add more
+
+  const isInvalidImage = invalidImagePatterns.some((pattern) =>
+    imageUrl.toLowerCase().includes(pattern),
+  );
+
+  return isInvalidImage ? fallbackImage : imageUrl;
+};
