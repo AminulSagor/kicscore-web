7a1f07d50c9d68d5d1612e1c0fda6a73533d218f
Shek Rasel <swe.rasel@gmail.com>
2026-05-19 12:48:54 +0600

    image configaration done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d2ml1l8vdyfdxz.cloudfront.net",
+      },
+      {
+        protocol: "https",
+        hostname: "www.sportsnet.ca",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
