1c3370b762531402e4da7f5949ac3356ed1c3ee5
Shek Rasel <swe.rasel@gmail.com>
2026-05-23 16:00:48 +0600

    retun image logic change

--- utils/image/image.utils.ts
+++ utils/image/image.utils.ts
@@ -1,15 +1,23 @@
+// import { IMAGE } from "@/constants/image.path";
+
+// export const getValidImage = (imageUrl?: string | null) => {
+//   const fallbackImage = IMAGE.fallback_image;
+
+//   if (!imageUrl) return fallbackImage;
+
+//   const invalidImagePatterns = ["favicon", ".ico", "sn_favicon"]; //you can add more
+
+//   const isInvalidImage = invalidImagePatterns.some((pattern) =>
+//     imageUrl.toLowerCase().includes(pattern),
+//   );
+
+//   return isInvalidImage ? fallbackImage : imageUrl;
+// };
+
 import { IMAGE } from "@/constants/image.path";
 
-export const getValidImage = (imageUrl?: string | null) => {
-  const fallbackImage = IMAGE.fallback_image;
+export const getValidImage = (imageUrl?: string | null): string => {
+  if (!imageUrl) return IMAGE.fallback_image;
 
-  if (!imageUrl) return fallbackImage;
-
-  const invalidImagePatterns = ["favicon", ".ico", "sn_favicon"]; //you can add more
-
-  const isInvalidImage = invalidImagePatterns.some((pattern) =>
-    imageUrl.toLowerCase().includes(pattern),
-  );
-
-  return isInvalidImage ? fallbackImage : imageUrl;
+  return imageUrl;
 };
