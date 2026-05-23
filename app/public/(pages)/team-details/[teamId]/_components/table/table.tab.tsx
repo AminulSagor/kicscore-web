812f05a9b3a987a363fd8eb97a598f268e521a11
MeherunNessaSuchana <mnsuchana0922@gmail.com>
2026-05-24 02:49:10 +0600

    feat: integrate team standings table API

--- app/public/(pages)/team-details/[teamId]/_components/table/table.tab.tsx
+++ app/public/(pages)/team-details/[teamId]/_components/table/table.tab.tsx
@@ -1,7 +1,16 @@
 import StandingsTable from "@/app/public/(pages)/league-details/[leagueId]/_components/table/standings-table";
+import type { LeagueStandingTeam } from "@/types/football/leagues/league.standings";
 
-const TableTab = () => {
-  return <div className="mt-6 sm:mt-8">{/* <StandingsTable /> */}</div>;
+type Props = {
+  teams: LeagueStandingTeam[];
+};
+
+const TableTab = ({ teams }: Props) => {
+  return (
+    <div className="mt-6 sm:mt-8">
+      <StandingsTable teams={teams} />
+    </div>
+  );
 };
 
 export default TableTab;
